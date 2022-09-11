0f437b4bcbd3358a0d0fb47ffdea714ea3910aba
Kathleen Jogno <jognokath@gmail.com>
2022-09-11 21:21:34 +0800

    Added eventlisteners for choosing meal

---
+++ js/main.js
@@ -0,0 +1,94 @@
+// meal choices
+const breakfast = document.querySelector('#meal1');
+const lunch = document.querySelector('#meal2');
+const dinner = document.querySelector('#meal3');
+const dessert = document.querySelector('#meal4');
+
+// cards for foods
+const food1 = document.querySelector('#card1');
+const food2 = document.querySelector('#card2');
+const food3 = document.querySelector('#card3');
+const food4 = document.querySelector('#card4');
+
+const hoverFood1Breakfast = () => {
+    food1.style.cssText = 'filter: brightness(50%); padding: 50px 0;'
+    // document.styleSheets[0].insertRule('#food1::after { content: ""; opacity: 0.5; }', 0);
+    food1.innerHTML = '<h2>$14.99</h2><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><p>Rice + Miso Soup + Salmon</p><button>Order Now</button>'
+    console.log('works liek a charm')
+}
+
+const outFood1Breakfast = () => {
+    food1.style.removeProperty('filter');
+    console.log('works liek aasa charm')
+    food1.innerHTML = ''
+}
+
+const chooseBreakfast = () => {
+    breakfast.setAttribute("id", "selectedMeal");
+    lunch.setAttribute("id", "");
+    dinner.setAttribute("id", "");
+    dessert.setAttribute("id", "");
+
+    food1.style.cssText = 'background-image: url("./img/breakfast/b01.png"); background-repeat: no-repeat; background-position: center center; '
+    food2.style.cssText = 'background-image: url("./img/breakfast/b02.png"); background-repeat: no-repeat; background-position: center center; '
+    food3.style.cssText = 'background-image: url("./img/breakfast/b03.png"); background-repeat: no-repeat; background-position: center center; '
+    food4.style.cssText = 'background-image: url("./img/breakfast/b04.png"); background-repeat: no-repeat; background-position: center center; '
+
+    food1.addEventListener("mouseover", hoverFood1Breakfast);
+    food1.addEventListener("mouseout", outFood1Breakfast);
+
+
+}
+
+const chooseLunch = () => {
+    lunch.setAttribute("id", "selectedMeal");
+    breakfast.setAttribute("id", "");
+    dinner.setAttribute("id", "");
+    dessert.setAttribute("id", "");
+
+    food1.style.cssText = 'background-image: url("./img/lunch/l01.png"); background-repeat: no-repeat; background-position: center center; '
+    food2.style.cssText = 'background-image: url("./img/lunch/l02.png"); background-repeat: no-repeat; background-position: center center; '
+    food3.style.cssText = 'background-image: url("./img/lunch/l03.png"); background-repeat: no-repeat; background-position: center center; '
+    food4.style.cssText = 'background-image: url("./img/lunch/l04.png"); background-repeat: no-repeat; background-position: center center; '
+}
+
+const chooseDinner = () => {
+    dinner.setAttribute("id", "selectedMeal");
+    breakfast.setAttribute("id", "");
+    lunch.setAttribute("id", "");
+    dessert.setAttribute("id", "");
+
+
+    food1.style.cssText = 'background-image: url("./img/dinner/d01.png"); background-repeat: no-repeat; background-position: center center; '
+    food2.style.cssText = 'background-image: url("./img/dinner/d02.png"); background-repeat: no-repeat; background-position: center center; '
+    food3.style.cssText = 'background-image: url("./img/dinner/d03.png"); background-repeat: no-repeat; background-position: center center; '
+    food4.style.cssText = 'background-image: url("./img/dinner/d04.png"); background-repeat: no-repeat; background-position: center center; '
+}
+const chooseDessert = () => {
+    dessert.setAttribute("id", "selectedMeal");
+    breakfast.setAttribute("id", "");
+    lunch.setAttribute("id", "");
+    dinner.setAttribute("id", "");
+
+    food1.style.cssText = 'background-image: url("./img/dessert/ds01.png"); background-repeat: no-repeat; background-position: center center; '
+    food2.style.cssText = 'background-image: url("./img/dessert/ds02.png"); background-repeat: no-repeat; background-position: center center; '
+    food3.style.cssText = 'background-image: url("./img/dessert/ds03.png"); background-repeat: no-repeat; background-position: center center; '
+    food4.style.cssText = 'background-image: url("./img/dessert/ds04.png"); background-repeat: no-repeat; background-position: center center; '
+}
+// const meals = [breakfast, lunch, dinner, dessert];
+// meals.forEach(meal => meal.addEventListener('click', chooseBreakfast));
+
+breakfast.addEventListener('click', chooseBreakfast);
+lunch.addEventListener('click', chooseLunch);
+dinner.addEventListener('click', chooseDinner);
+dessert.addEventListener('click', chooseDessert)
+
+// on hover eventlisteners
+
+
+
+if (breakfast.id == 'selectedMeal') {
+
+
+
+}
